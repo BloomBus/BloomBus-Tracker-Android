67fb5fef0b7b9abc5992c5dc062e9966bd437b91
John Gibson <dev@johngibson.me>
2019-05-10 20:55:23 -0400

    Fix lat-lng coordinates order in shuttle geojson

--- app/src/main/java/edu/bloomu/bloombus/bloombus_tracker_android/GeoJSONGeometry.java
+++ app/src/main/java/edu/bloomu/bloombus/bloombus_tracker_android/GeoJSONGeometry.java
@@ -2,12 +2,15 @@ package edu.bloomu.bloombus.bloombus_tracker_android;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class GeoJSONGeometry {
     public String type;
-    public LatLng coordinates;
+    public List<Double> coordinates;
 
     public GeoJSONGeometry(String type, LatLng coordinates) {
         this.type = type;
-        this.coordinates = coordinates;
+        this.coordinates = Arrays.asList(coordinates.longitude, coordinates.latitude);
     }
 }
