66317818a1ae040054a55b9110ed482c67792d6a
Michael O'Donnell <43424718+michaelpodonnell@users.noreply.github.com>
2019-10-19 14:09:12 -0400

    Change sessionID to String

--- app/src/main/java/edu/bloomu/bloombus/bloombus_tracker_android/HistoricalLogInformation.java
+++ app/src/main/java/edu/bloomu/bloombus/bloombus_tracker_android/HistoricalLogInformation.java
@@ -1,16 +1,14 @@
 package edu.bloomu.bloombus.bloombus_tracker_android;
 
-import java.util.UUID;
-
 public class HistoricalLogInformation {
     public long arrivalTime;
     public String prevStop;
     public String nextStop;
     public String loopKey;
-    public UUID sessionID;
+    public String sessionID;
 
     public HistoricalLogInformation(
-        long arrivalTime, String prevStop, String nextStop, String loopKey, UUID sessionID
+        long arrivalTime, String prevStop, String nextStop, String loopKey, String sessionID
     ) {
         this.arrivalTime = arrivalTime;
         this.prevStop = prevStop;
