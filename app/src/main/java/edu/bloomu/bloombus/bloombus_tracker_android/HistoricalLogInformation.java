28388b5fab767c0e6b5485c04cb210a8fa6612cf
Michael O'Donnell <43424718+michaelpodonnell@users.noreply.github.com>
2019-10-19 13:59:02 -0400

    Add sessionID to HistoricalLogInformation

--- app/src/main/java/edu/bloomu/bloombus/bloombus_tracker_android/HistoricalLogInformation.java
+++ app/src/main/java/edu/bloomu/bloombus/bloombus_tracker_android/HistoricalLogInformation.java
@@ -1,17 +1,21 @@
 package edu.bloomu.bloombus.bloombus_tracker_android;
 
+import java.util.UUID;
+
 public class HistoricalLogInformation {
     public long arrivalTime;
     public String prevStop;
     public String nextStop;
     public String loopKey;
+    public UUID sessionID;
 
     public HistoricalLogInformation(
-        long arrivalTime, String prevStop, String nextStop, String loopKey
+        long arrivalTime, String prevStop, String nextStop, String loopKey, UUID sessionID
     ) {
         this.arrivalTime = arrivalTime;
         this.prevStop = prevStop;
         this.nextStop = nextStop;
         this.loopKey = loopKey;
+        this.sessionID = sessionID;
     }
 }
